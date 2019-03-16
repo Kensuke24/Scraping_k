673b54411fec2f23c326497477218f55b66023fc
Kensuke24 <hibis.tori@gmail.com>
2019-03-17 08:44:49 +0900

    抽出方法の変更

--- KowaItems.py
+++ KowaItems.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 
-df_header = pd.read_excel('data/3.15_製品データベース.xlsx',encoding="UTF-8")
-print(df_header)
+df = pd.read_excel('data/3.15_製品データベース.xlsx',encoding="UTF-8")
+print(df.query('cable.str.contains("アルミ")', engine='python'))   # 列名cableにアルミが含まれる行の抽出
