0a1daf2d7d025740df954530762ec074ebddc56b
Kensuke24 <hibis.tori@gmail.com>
2019-03-17 11:02:47 +0900

    抽出条件の追加

--- KowaItems.py
+++ KowaItems.py
@@ -7,7 +7,7 @@ df = pd.read_excel('data/3.15_製品データベース.xlsx',encoding="UTF-8")
 # print(df.query('55 <= min1 < 80'))  # 列名min1の数値が範囲が55～80の行の抽出
 
 size1 = 38
-size2 = 5
+size2 = 14
 size = (df.query('min1 <= @size1 & max1 >= @size1 & min2 <= @size2 & max2 >= @size2'))
+print(size.query('cable.str.contains("アルミ")', engine='python'))
 
-print(size)
