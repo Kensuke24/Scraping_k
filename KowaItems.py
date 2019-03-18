7a6ae422c3788aed4cbe23b72ad5c20d66201089
Kensuke24 <hibis.tori@gmail.com>
2019-03-18 23:46:50 +0900

    データファイルの移動

--- KowaItems.py
+++ KowaItems.py
@@ -6,7 +6,7 @@ https://note.nkmk.me/python-pandas-query/
 import pandas as pd
 
 
-df = pd.read_excel('data/3.15_製品データベース.xlsx',encoding="UTF-8")
+df = pd.read_excel('data/3.18_製品データベース.xlsx',encoding="UTF-8")
 # print(df.query('cable.str.contains("アルミ")', engine='python'))   # 列名cableにアルミが含まれる行の抽出
 
 # print(df.query('55 <= min1 < 80'))  # 列名min1の数値が範囲が55～80の行の抽出
